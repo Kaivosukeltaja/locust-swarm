9b847a77cddf84b44a64ee5556b6b538a2ab2690
Niko Salminen <niko@salminen.me>
2015-12-17 12:47:52 +0200

    Bootstrapping support for yum based AIMs

--- example/bootstrap-master/bootstrap.sh
+++ example/bootstrap-master/bootstrap.sh
@@ -1,12 +1,25 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-sudo apt-get update -y
-sudo apt-get upgrade -y
-sudo apt-get install python -y
-sudo apt-get install python-pip -y
-sudo apt-get install python-dev -y
-sudo apt-get install build-essential -y
-sudo apt-get install git -y
-sudo apt-get install libevent-dev -y
-sudo pip install --upgrade pip
-sudo pip install -r /tmp/locust/bootstrap-master/requirements.txt
+if [ -n "$(command -v yum)" ]
+then
+  # TODO: Install python and pip if your AMI doesn't include them already
+  sudo yum install gcc gcc-c++ make openssl-devel -y
+  sudo yum install git -y
+  sudo pip install --upgrade pip
+  # TODO: The upgrade screws up the directory of PIP for some reason
+  sudo /usr/local/bin/pip install -r /tmp/locust/bootstrap-master/requirements.txt
+elif [ -n "$(command -v apt-get)" ]
+  sudo apt-get update -y
+  sudo apt-get upgrade -y
+  sudo apt-get install python -y
+  sudo apt-get install python-pip -y
+  sudo apt-get install python-dev -y
+  sudo apt-get install build-essential -y
+  sudo apt-get install git -y
+  sudo apt-get install libevent-dev -y
+  sudo pip install --upgrade pip
+  sudo pip install -r /tmp/locust/bootstrap-master/requirements.txt
+else
+  echo "Error: Can't find either yum or apt-get!"
+  echo "Please modify bootstrap.sh to support your weird package manager."
+fi
